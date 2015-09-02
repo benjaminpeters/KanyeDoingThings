186014ef44dcbef149ba6c7155d577c92f137553
benjamin peters <benpeters49@gmail.com>
2015-09-02 13:02:56 -0400

    Initial Commit

---
+++ KanyeDoingThings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KanyeDoingThings
+//
+//  Created by Ben Peters on 2015-09-02.
+//  Copyright (c) 2015 Orange Chips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
