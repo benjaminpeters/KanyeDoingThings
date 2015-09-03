79444ac9e23f8d7124fd2523c88ba119e21ff515
benjamin peters <benpeters49@gmail.com>
2015-09-02 20:48:47 -0400

    Kanye added, with pan, pinch and rotate gestures. App is currently for iPhone only
    TODO
    - add different Kanyes to pick and choose from, set it up like the emoji picker on snapchat
    - add the option to save the photos and share to social media (insta, Facebook, twitter)
    - make it look pretty, custom icon etc

--- KanyeDoingThings/ViewController.swift
+++ KanyeDoingThings/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.delegate = self
     }
     
-    // MARK: - UIImagePickerControllerDelegate Methods
+    // Pick a background from the PhotoLibrary
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
@@ -40,5 +40,54 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // Handle the PanGesture for a Kanye
+    @IBAction func handlePan(recognizer: UIPanGestureRecognizer){
+        let translations = recognizer.translationInView(self.view)
+        
+        if let view = recognizer.view{
+            view.center = CGPoint(x:view.center.x + translations.x, y:view.center.y + translations.y)
+        }
+        recognizer.setTranslation(CGPointZero, inView: self.view)
+        
+        if recognizer.state == UIGestureRecognizerState.Ended {
+            // 1
+            let velocity = recognizer.velocityInView(self.view)
+            let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+            let slideMultiplier = magnitude / 200
+            println("magnitude: \(magnitude), slideMultiplier: \(slideMultiplier)")
+            
+            // 2
+            let slideFactor = 0.1 * slideMultiplier     //Increase for more of a slide
+            // 3
+            var finalPoint = CGPoint(x:recognizer.view!.center.x + (velocity.x * slideFactor),
+                y:recognizer.view!.center.y + (velocity.y * slideFactor))
+            // 4
+            finalPoint.x = min(max(finalPoint.x, 0), self.view.bounds.size.width)
+            finalPoint.y = min(max(finalPoint.y, 0), self.view.bounds.size.height)
+            
+            // 5
+            UIView.animateWithDuration(Double(slideFactor * 2),
+            delay: 0,
+                // 6
+            options: UIViewAnimationOptions.CurveEaseOut,
+            animations: {recognizer.view!.center = finalPoint },
+            completion: nil)
+        }
+    }
+    @IBAction func handlePinch(recognizer : UIPinchGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = CGAffineTransformScale(view.transform,
+                recognizer.scale, recognizer.scale)
+            recognizer.scale = 1
+        }
+    }
+    
+    @IBAction func handleRotate(recognizer : UIRotationGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = CGAffineTransformRotate(view.transform, recognizer.rotation)
+            recognizer.rotation = 0
+        }
     }
 }
